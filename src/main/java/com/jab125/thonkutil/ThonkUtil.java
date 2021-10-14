e294ae65a26552a0878696818b94705ba0d317fb
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-10-14 16:29:16 +1100

    Add Potion Recipes

--- src/main/java/com/jab125/thonkutil/ThonkUtil.java
+++ src/main/java/com/jab125/thonkutil/ThonkUtil.java
@@ -1,6 +1,11 @@
 package com.jab125.thonkutil;
 
+import com.jab125.thonkutil.util.PotionUtil;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,7 +13,7 @@ public class ThonkUtil implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger("thonkutil");
 
 	@Override
 	public void onInitialize() {
