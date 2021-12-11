d5f23f2a50e5f75d6842f4edcd45d3556aff328c
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-12-11 11:46:27 +1100

    Rename some variables

--- thonkutil-base/src/main/java/com/jab125/thonkutil/ThonkUtil.java
+++ thonkutil-base/src/main/java/com/jab125/thonkutil/ThonkUtil.java
@@ -7,8 +7,9 @@ import com.google.gson.GsonBuilder;
 public class ThonkUtil {
     public static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).setPrettyPrinting().create();
     public static class MODID {
-        public static final String POTION_API_V0_MODID = "thonkutil-potions-v0";
+        public static final String POTIONS_V0_MODID = "thonkutil-potions-v0";
         public static final String COORDS_V1_MODID = "thonkutil-coords-v1";
         public static final String TRADES_V1_MODID = "thonkutil-trades-v1";
+        public static final String BASE_MODID = "thonkutil-base";
     }
 }
