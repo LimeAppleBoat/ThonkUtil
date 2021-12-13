cfcde2a03b8c5a83ee86492ac62abe0851fa6077
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-12-13 17:28:10 +1100

    Add some registry shortcuts

--- thonkutil-base/src/main/java/com/jab125/thonkutil/util/Util.java
+++ thonkutil-base/src/main/java/com/jab125/thonkutil/util/Util.java
@@ -1,6 +1,10 @@
 package com.jab125.thonkutil.util;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -58,4 +62,27 @@ public class Util {
         return new ItemStack(item, count);
     }
 
+    public static JointBlockItem registerBlockWithItem(Identifier identifier, Block block, net.minecraft.item.Item.Settings settings) {
+        return JointBlockItem.register(identifier, block, settings);
+    }
+
+    public static class JointBlockItem {
+        private final net.minecraft.item.Item item;
+        private final Block block;
+        private JointBlockItem(net.minecraft.item.Item item, Block block) {
+            this.item = item;
+            this.block = block;
+        }
+        private static JointBlockItem register(Identifier identifier, Block block, net.minecraft.item.Item.Settings settings) {
+            return new JointBlockItem(Registry.register(Registry.ITEM, identifier, new BlockItem(block, settings)), Registry.register(Registry.BLOCK, identifier, block));
+        }
+
+        public Block getBlock() {
+            return block;
+        }
+
+        public net.minecraft.item.Item getItem() {
+            return item;
+        }
+    }
 }
