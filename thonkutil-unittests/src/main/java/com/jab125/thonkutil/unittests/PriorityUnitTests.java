c50dd4dfeadf4b4970af30220830a8c2ebb22b95
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-03-06 12:45:41 +1100

    add unit tests for prioritised SubscribeEvents

---
+++ thonkutil-unittests/src/main/java/com/jab125/thonkutil/unittests/PriorityUnitTests.java
@@ -0,0 +1,29 @@
+package com.jab125.thonkutil.unittests;
+
+import com.jab125.thonkutil.api.annotations.SubscribeEvent;
+import com.jab125.thonkutil.api.events.EventTaxi;
+import com.jab125.thonkutil.api.events.EventTaxiEvent;
+
+public class PriorityUnitTests {
+    public static void main(String[] args) {
+        EventTaxi.registerEventTaxiSubscriber(PriorityUnitTests.class);
+        EventTaxi.executeEventTaxi(new PriorityEvent());
+    }
+
+    @SubscribeEvent(priority = SubscribeEvent.Priority.LOW)
+    public static void execute1(PriorityEvent event) {
+        System.out.println(1);
+    }
+
+    @SubscribeEvent
+    public static void execute2(PriorityEvent event) {
+        System.out.println(2);
+    }
+
+    @SubscribeEvent
+    public static void execute3(PriorityEvent event) {
+        System.out.println(3);
+    }
+
+    public static class PriorityEvent extends EventTaxiEvent {}
+}
