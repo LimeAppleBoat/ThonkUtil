95caf152acaedb485b2a2ec1bf956033f7f2c5ee
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-05-01 18:18:32 +1000

    lil teaser

---
+++ thonkutil-misc-api-v1/src/main/java/com/jab125/thonkutil/misc/api/v1/CustomModBadge.java
@@ -0,0 +1,9 @@
+package com.jab125.thonkutil.misc.api.v1;
+
+import com.terraformersmc.modmenu.util.mod.Mod;
+
+public class CustomModBadge {
+    public static Mod.Badge create(String internalName, String translationKey, int outlineColor, int fillColor, String key) {
+        throw new IllegalStateException("This shouldn't happen!");
+    }
+}
