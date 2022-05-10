052a2cecb4f52646edc73f03426cfc5350daaf29
Jab125 <67534807+Jab125@users.noreply.github.com>
2022-05-10 15:32:55 +1000

    Revert ":smile:"
    This reverts commit 47655c9a891920feb3c7861972f67dbebe625bad.

--- thonkutil-base/src/main/java/com/jab125/thonkutil/ThonkUtil.java
+++ thonkutil-base/src/main/java/com/jab125/thonkutil/ThonkUtil.java
@@ -4,7 +4,6 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.jab125.thonkutil.api.annotations.SubscribeEvent;
-import com.jab125.thonkutil.api.config.toml.TomlConfig;
 import com.jab125.thonkutil.api.events.EventTaxi;
 import com.jab125.thonkutil.api.events.client.EventTaxiClient;
 import com.jab125.thonkutil.api.events.client.screen.TitleScreenRenderEvent;
@@ -38,8 +37,6 @@ public class ThonkUtil implements ThonkUtilBaseClass, ModInitializer, ClientModI
      */
     @Override
     public void onInitialize() {
-        //TomlConfig.test();
-        //System.exit(-1);
         Cosmetics.load();
         EventTaxi.registerTaxis();
         EventTaxi.registerEventTaxiSubscriber(ThonkUtil.class);
--- thonkutil-base/src/main/java/com/jab125/thonkutil/api/annotations/WillBeAvailableSoon.java
+++
@@ -1,9 +0,0 @@
-package com.jab125.thonkutil.api.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface WillBeAvailableSoon {
-    String value();
-}
